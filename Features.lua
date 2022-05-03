b205370a3d477456991a319e60765d3471abdf72
WobbyChip <wobbychip.simik@gmail.com>
2022-05-03 16:09:26 +0300

    Update Features.lua

--- Features.lua
+++ Features.lua
@@ -82,7 +82,4 @@ local TeleportsList = Teleports.self:create("Box", {
 local TeleportsNew = Teleports.self:create("Input", {
     Name = "Create New",
     Default = "Location 1",
-    Callback = function(value)
-        
-    end,
 })
