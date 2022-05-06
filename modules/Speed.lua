2e53d22ec2126f7e0ddb1c6174ff962e1091101e
WobbyChip <wobbychip.simik@gmail.com>
2022-05-07 01:36:35 +0300

    Update Speed.lua

--- modules/Speed.lua
+++ modules/Speed.lua
@@ -15,15 +15,19 @@ local _Speed = (function()
         if enabled then
             module.Options.Saved = character.Humanoid.WalkSpeed
             character.Humanoid.WalkSpeed = module.Options.Speed
-        else
+        elseif module.Options.Enabled then
             character.Humanoid.WalkSpeed = module.Options.Saved
         end
 
         module.Options.Enabled = enabled
     end
 
-    module.setSpeed = function()
-        return game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame
+    module.setSpeed = function(value)
+        local enabled = module.Options.Enabled
+        module.Options.Enabled = false
+        module.Options.Speed = value
+        character.Humanoid.WalkSpeed = value
+        module.Options.Enabled = enabled
     end
 
     Player.CharacterAdded:Connect(function(char)
